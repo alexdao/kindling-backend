b227a6c2e9ca39775cde24a1d4d507c1f7a04276
Alex Dao <azd2@duke.edu>
2016-11-20 09:39:13 -0500

    test

--- index.js
+++ index.js
@@ -134,6 +134,7 @@ io.on('connection', function(socket){
         if (currChat.containsUser(socket)) {
           // send the partner a disconnect message
           let partner = currChat.getPartner(socket);
+          console.log('found a private chat i belong to');
           if (partner != null) {
             console.log('found user to disconnect from');
             partner.socket.emit('disconnect', currChat.getChatId());
