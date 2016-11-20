0dbf0b09fb656147f91e140a5cbe2d8561fbbe27
Alex Dao <azd2@duke.edu>
2016-11-20 09:54:19 -0500

    change dc name

--- index.js
+++ index.js
@@ -137,7 +137,7 @@ io.on('connection', function(socket){
           console.log('found a private chat i belong to');
           if (partner != null) {
             console.log('found user to disconnect from');
-            partner.emit('disconnect', currChat.getChatId());
+            partner.emit('disconnect_client', currChat.getChatId());
           }
 
           // delete chat from map
