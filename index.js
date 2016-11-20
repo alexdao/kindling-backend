c39c729cdcb794058b07d653da4a09a7fa858cb1
Alex Dao <azd2@duke.edu>
2016-11-19 23:50:06 -0500

    Remove manual port setting on remote server

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 'use strict';
 const app = require('express')();
+app.set('port', (process.env.PORT || 5000));
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const Article = require('./Article');
