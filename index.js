1d843988471ddc5bf42eabe5b0b6f8bae3488d1a
Alex Dao <azd2@duke.edu>
2016-11-20 09:45:45 -0500

    fix socket bug

--- index.js
+++ index.js
@@ -137,7 +137,7 @@ io.on('connection', function(socket){
           console.log('found a private chat i belong to');
           if (partner != null) {
             console.log('found user to disconnect from');
-            partner.socket.emit('disconnect', currChat.getChatId());
+            partner.sock.emit('disconnect', currChat.getChatId());
           }
 
           // delete chat from map
